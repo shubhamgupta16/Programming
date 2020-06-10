4f68492dd8b62c0241845248fb0d6268952e2b1f
shubhamgupta16 <guptashubham7777@gmail.com>
2020-06-10 19:18:19 +0530

    add a program of Greatest Common Factor

---
+++ Python Programs/gcd.py
@@ -0,0 +1,23 @@
+# Programe for find Greatest common factor
+
+def gcd(m,n):
+    fm = []
+    for i in range(1, m+1):
+        if(m%i) == 0:
+            fm.append(i)
+
+    fn = []
+    for j in range(1, n+1):
+        if(n%j) == 0:
+            fn.append(j)
+
+    cf = []
+    for f in fm:
+        if f in fn:
+            cf.append(f)
+
+    return(cf[-1])
+
+# call gcd function
+num = gcd(14,63)
+print(num)
